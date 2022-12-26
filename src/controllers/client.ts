d712d99714560d5987d86b71d7100df37e3832de
Lekkyjay <lekkyjay@yahoo.com>
2022-12-26 02:36:00 +0100

    Fixed sorting issue.

--- src/controllers/client.ts
+++ src/controllers/client.ts
@@ -4,6 +4,7 @@ import ProductStat from '../models/ProductStat'
 import User from '../models/User'
 import Transaction from '../models/Transaction'
 import { countryToAlpha2, countryToAlpha3 } from 'country-to-iso'
+import { SortOrder } from 'mongoose'
 // import getCountryIso3 from 'country-iso-2-to-3'  
 
 export const getProducts = async (req: Request, res: Response) => {
@@ -35,21 +36,34 @@ export const getCustomers = async (req: Request, res: Response) => {
   }
 }
 
+type ISort = [{
+  field: string
+  sort: string
+}]
+
 export const getTransactions = async (req: Request, res: Response) => {
   try {
     // sort should look like this: { 'field': 'userId', 'sort': 'desc'}
     const { page = 1, pageSize = 20, sort = null, search = '' } = req.query
+    // console.log('req.query:', req.query)      //sort: '[{"field":"userId","sort":"asc"}]'
 
     // formatted sort should look like { userId: -1 }
     const generateSort = () => {
       const sortParsed = JSON.parse(sort as string)     //converts JSON string to JS obj.
+      // console.log({sortParsed})       // sortParsed: [ { field: 'userId', sort: 'asc' } ]
+      const [ sortObj ] = sortParsed  //destructured obj out of array into sortObj variable
+      const field = sortObj.field
+      const sortKey = sortObj.sort
       const sortFormatted = {
-        [sortParsed.field]: (sortParsed.sort = 'asc' ? 1 : -1),
+        // [sortParsed.field as keyof ISort]: (sortParsed.sort = 'asc' ? 1 : -1) as SortOrder 
+        // [sortParsed.field]: (sortParsed.sort = 'asc' ? 1 : -1) as SortOrder 
+        [field]: (sortKey == 'asc' ? 1 : -1) as SortOrder 
       }
 
       return sortFormatted
     }
     const sortFormatted = Boolean(sort) ? generateSort() : {}
+    // console.log({sortFormatted})      //sortFormatted: { _id: -1 }
 
     const transactions = await Transaction.find({
       $or: [
@@ -57,13 +71,12 @@ export const getTransactions = async (req: Request, res: Response) => {
         { userId: { $regex: new RegExp(search as string, 'i') } },
       ],
     })
-      // .sort(sortFormatted)
-      .sort({})
+      .sort(sortFormatted)
       .skip(Number(page) * Number(pageSize))
       .limit(Number(pageSize))
 
     const total = await Transaction.countDocuments({
-      name: { $regex: search, $options: 'i' },
+      // name: { $regex: search, $options: 'i' },
     })
 
     res.status(200).json({
