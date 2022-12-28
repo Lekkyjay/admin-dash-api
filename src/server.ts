fea818064c6cc0006837de5d45347bfd95bd91a9
Lekkyjay <lekkyjay@yahoo.com>
2022-12-28 19:06:21 +0100

    Changed PORT.

--- src/server.ts
+++ src/server.ts
@@ -43,7 +43,7 @@ app.use('/management', managementRoutes)
 app.use('/sales', salesRoutes)
 
 /* MONGOOSE SETUP */
-const PORT = process.env.PORT || 9000
+const PORT = process.env.PORT || 5000
 
 mongoose.set('strictQuery', false)
 mongoose
